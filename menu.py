863d31556912ebeee6f4dfb2218c345b3c095dd1
jringenbach <ringenbach.julien@gmail.com>
2019-06-17 12:39:52 +0200

    Selection language menu works

--- menu.py
+++ menu.py
@@ -21,6 +21,35 @@ class Menu:
 
 
 
+    def change_language(self, language):
+        """Change the language in conf.json if it is necessary
+        language : language chosen by the user (string)"""
+
+        #We try to open conf.json in reading mode to read its content
+        try:
+            with open("conf.json", "r", encoding="utf-8") as file:
+                file_loaded = json.load(file)
+                file.close()
+                
+        #If the file couldn't be opened   
+        except:
+            print("Couldn't open file : conf.json")
+
+        #If the language in conf.json is different from the one selected by the user
+        if file_loaded["language"] != "language":
+            
+            try:
+                with open("conf.json", "w", encoding="utf-8") as file:
+
+                        file_loaded["language"] = language
+                        file_loaded = json.dumps(file_loaded, indent=4)
+                        file.write(file_loaded)
+                        file.close()
+            except:
+                print("Couldn't open file : conf.json")
+
+
+
     def display(self):
         """Display the options of the menu in the terminal"""
 
@@ -30,7 +59,7 @@ class Menu:
         else:
             #We display each option one by one
             for option in self.options:
-                print(option)
+                print(option+"."+self.options[option])
 
 
 
@@ -41,8 +70,10 @@ class Menu:
 
         #We try to open language.json to get the options of the menu
         try:
-            with open("language.json") as menu_json_file:
+            with open("language.json", encoding="utf-8") as menu_json_file:
                 menu_json = json.load(menu_json_file)
+                print(menu_json)
+                print(type(menu_json))
                 menu_json_file.close()
                 return menu_json[self.language][menu_name]
 
@@ -57,7 +88,7 @@ class Menu:
 
         #We try to open conf.json to get the language
         try:
-            with open("conf.json") as language_json_file:
+            with open("conf.json", encoding="utf-8") as language_json_file:
                 json_lang = json.load(language_json_file)
                 language_json_file.close()
                 return json_lang["language"]
--- run.py
+++ run.py
@@ -6,7 +6,20 @@ from menu import Menu
 #Project classes and libraries
 from link_db import Link_DB
 
+#Instanciation of the language choice menu
 language_menu = Menu("language_menu")
-language_menu.display()
+language = str()
 
-Link_DB.connexion_with_conf_file("conf.json")
+#While the user doesn't chose fr or en, we keep asking him the language
+while(language != "fr" and language != "en"):
+    language_menu.display()
+    language = input(" => ")
+
+#We change the language in the conf.json if it is necessary
+language_menu.change_language(language)
+
+#We instanciate the main menu
+main_menu = Menu("main_menu")
+main_menu.display()
+
+#Link_DB.connexion_with_conf_file("conf.json")
