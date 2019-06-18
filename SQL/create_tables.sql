382d28a41b6f73ae3f4ad91cfb2d629190e98eef
jringenbach <ringenbach.julien@gmail.com>
2019-06-18 12:06:00 +0200

    Scripts SQL created and MLD schema created

---
+++ SQL/create_tables.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE IF NOT EXISTS openfoodfacts;
+USE openfoodfacts;
+
+CREATE TABLE IF NOT EXISTS Brands(
+id_brands INT AUTO_INCREMENT PRIMARY KEY,
+brands_tags VARCHAR(30) NOT NULL)
+ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS Nutrition_grade(
+nutrition_grade VARCHAR(1) PRIMARY KEY
+)
+ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS Categories(
+id_categories INT AUTO_INCREMENT PRIMARY KEY,
+categorie_name VARCHAR(50) NOT NULL
+)
+ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS Products(
+id_products INT AUTO_INCREMENT PRIMARY KEY,
+product_name_fr VARCHAR(60) NOT NULL,
+url VARCHAR(40) NOT NULL
+)
+ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS Products_Brands(
+id_products INT,
+id_brands INT
+)
+ENGINE=InnoDB;
+
+ALTER TABLE Products_Brands
+ADD CONSTRAINT fk_id_products_brands FOREIGN KEY (id_products) REFERENCES Products(id_products),
+ADD CONSTRAINT fk_id_brands FOREIGN KEY (id_brands) REFERENCES Brands(id_brands);
+
+CREATE TABLE IF NOT EXISTS Products_Nutrition_grade(
+id_products INT,
+nutrition_grade VARCHAR(1)
+)
+ENGINE=InnoDB;
+
+ALTER TABLE Products_Nutrition_grade
+ADD CONSTRAINT fk_id_products_nutrition_grade FOREIGN KEY (id_products) REFERENCES Products(id_products),
+ADD CONSTRAINT fk_id_nutrition_grade FOREIGN KEY(nutrition_grade) REFERENCES Nutrition_grade(nutrition_grade);
+
+CREATE TABLE IF NOT EXISTS Products_Categories(
+id_products INT,
+id_categories INT
+)
+ENGINE=InnoDB;
+
+ALTER TABLE Products_Categories
+ADD CONSTRAINT fk_id_products_categories FOREIGN KEY (id_products) REFERENCES Products(id_products),
+ADD CONSTRAINT fk_id_categories FOREIGN KEY(id_categories) REFERENCES Categories(id_categories);
---
+++ SQL/delete_tables.sql
@@ -0,0 +1,20 @@
+ALTER TABLE Products_Brands
+DROP FOREIGN KEY fk_id_products_brands,
+DROP FOREIGN KEY fk_id_brands;
+
+ALTER TABLE Products_Nutrition_grade
+DROP FOREIGN KEY fk_id_products_nutrition_grade,
+DROP FOREIGN KEY fk_id_nutrition_grade;
+
+ALTER TABLE Products_Categories
+DROP FOREIGN KEY fk_id_products_categories,
+DROP FOREIGN KEY fk_id_categories;
+
+DROP TABLE IF EXISTS Products_Brands;
+DROP TABLE IF EXISTS Products_Nutrition_grade;
+DROP TABLE IF EXISTS Products_Categories;
+
+DROP TABLE IF EXISTS Brands;
+DROP TABLE IF EXISTS Categories;
+DROP TABLE IF EXISTS Nutrition_grade;
+DROP TABLE IF EXISTS Products;
